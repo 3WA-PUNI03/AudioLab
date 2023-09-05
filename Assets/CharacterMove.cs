45dc352b464f94ef0627236c0d702eb457af0014
Kévin Roussel <9155865+KevinRoussel@users.noreply.github.com>
2023-09-05 15:36:52 +0200

    Add controller Sensibility

--- Assets/CharacterMove.cs
+++ Assets/CharacterMove.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +7,16 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class CharacterMove : MonoBehaviour
 {
-
     [SerializeField] InputActionReference _move;
     [SerializeField] InputActionReference _look;
     [SerializeField] CharacterController _controller;
     [SerializeField] float _speed;
 
     [SerializeField] Transform _cameraTransform;
+
+    [SerializeField] Vector2 _cameraSensibility;
+
+    [SerializeField] CinemachineVirtualCamera _vc;
 
     float _vertical;
     float _horizontal;
@@ -26,14 +30,17 @@ public class CharacterMove : MonoBehaviour
         Debug.Log(look);
 
         // On change notre rotation par rapport à Looks
-        _horizontal += look.x;
-        _vertical -= look.y;
+        _horizontal += look.x * _cameraSensibility.x;
+        _vertical -= look.y * _cameraSensibility.y;
+
         // On clamp l'axe vertical pour pas faire de looping
         _vertical = Mathf.Clamp(_vertical, -80, 80);
-
+        
+        // On applique la rotation droite/gauche à notre objet pour tourner tout le monde
         transform.rotation = Quaternion.Euler(0, _horizontal, 0);
+        // On applique la rotation haut/bas uniquement à notre camera pour qu'elle tourne seulesss
         _cameraTransform.localRotation = Quaternion.Euler(_vertical, 0, 0);
-
+        
     }
 
     private void MoveCharacter()
